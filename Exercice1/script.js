d84feb6b3a45f1dea4a6cb3f95b120f3f9b235b8
Anne-Lyse SCHILLING <annelyse.schilling@gmail.com>
2019-01-08 11:56:08 +0100

    first commit

---
+++ Exercice1/script.js
@@ -0,0 +1,15 @@
+function myFunction
+{
+  var dividende = document.GetElementbyID(firstNumber).value;
+  var diviseur = document.GetElementbyID(secondNumber).value;
+
+  var quotient = (dividende % diviseur);
+
+  alert(`Resultat = ${quotient}`);
+
+}
+//DOM pour récupérer le Diviseur et le Dividende
+
+//Récupérer la partie entière du quotient
+
+//
